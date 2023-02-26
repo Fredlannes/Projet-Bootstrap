5eba7fbdb15f22ae5cf50a8d5cb2f6ed8c0e48d1
Frederic Lannes <lannes.frederic@orange.fr>
2023-02-26 20:11:23 +0100

    add  button scroll-top

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const navbar = document.getElementById("navbarSite");
+const scrollTopButton = document.getElementById("scrollTopButton");
 
 window.onscroll=function(){
     if(window.pageYOffset>200){
         navbar.classList.remove("bg-tansparent", "navbar-dark");
         navbar.classList.add("bg-light", "navbar-light");
+        
+        scrollTopButton.classList.add("show");
     } else {
         navbar.classList.add("bg-tansparent", "navbar-dark");
         navbar.classList.remove("bg-light", "navbar-light");
+        
+        scrollTopButton.classList.remove("show");
     }
 }
 
